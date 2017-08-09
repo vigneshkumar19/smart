7877e9d8f2504d5090d050f3df3fd520b8419083
vigneshkumar19 <30831042+vigneshkumar19@users.noreply.github.com>
2017-08-09 13:02:29 +0530

    positiveornot.c

---
+++ positiveornot.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+printf("/n enter the number");
+if(a>0)
+{
+printf("the no is positive")
+}
+else
+{
+printf("the no is negative")
+}
+return 0;
+}
